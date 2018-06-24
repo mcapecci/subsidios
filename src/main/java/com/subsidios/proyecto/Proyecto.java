043ae70e74f98c7c4843d7af9bd799409a14659c
DIEGO <DIEGO@portatil>
2018-06-24 02:23:15 -0300

    Revert "Actualizacion data.sql Se puede agregar extension Postam: https://chrome.google.com/webstore/detail/postman/fhbjgbiflinjbdggehcddcbncdddomop?hl=es para probar servicios REST"
    This reverts commit 37039be4edc278f3bd150d487fd7bd58fe361ad0.

--- src/main/java/com/subsidios/proyecto/Proyecto.java
+++ src/main/java/com/subsidios/proyecto/Proyecto.java
@@ -10,28 +10,16 @@ public class Proyecto {
 	@GeneratedValue
 	private Long id;
 	private String nombre;
-	private int anioInicio;
-	private int anioFin;
 	
 	public Proyecto() {
 		super();
 	}
 
-	public Proyecto(Long id, String nombre, int anioInicio, int anioFin) {
+	public Proyecto(Long id, String nombre) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
-		this.anioInicio = anioInicio;
-		this.anioFin = anioFin;
 	}
-	
-	public Proyecto(String nombre, int anioInicio, int anioFin) {
-		super();
-		this.nombre = nombre;
-		this.anioInicio = anioInicio;
-		this.anioFin = anioFin;
-	}
-	
 	public Long getId() {
 		return id;
 	}
@@ -43,27 +31,5 @@ public class Proyecto {
 	}
 	public void setNombre(String name) {
 		this.nombre = name;
-	}
-
-	public int getAnioInicio() {
-		return anioInicio;
-	}
-
-	public void setAnioInicio(int anioInicio) {
-		this.anioInicio = anioInicio;
-	}
-
-	public int getAnioFin() {
-		return anioFin;
-	}
-
-	public void setAnioFin(int anioFin) {
-		this.anioFin = anioFin;
 	}		
-	
-	@Override
-	public String toString() {
-		return String.format("Proyecto [nombre=%s, anioInicio=%s, anioFin=%s]", nombre, anioInicio, anioFin);
-	}
-
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,13 @@
-insert into PROYECTO (id, nombre, anio_inicio, anio_fin)
+insert into PROYECTO
 values
-(1,'Proyecto 1',2017,2019),
-(2,'Proyecto 2',2018,2020),
-(3,'Proyecto 3',2018,2020),
-(4,'Proyecto 4',2017,2019);
+(1,'Proyecto 1'),
+(2,'Proyecto 2'),
+(3,'Proyecto 3'),
+(4,'Proyecto 4');
 
-insert into INCISO_PROYECTO (id, nombre, anio, monto, id_proyecto)
+insert into INCISO_PROYECTO
 values
-(1,'Inciso A',2017,4500.00,1),
-(2,'Inciso B',2017,1500.00,1),
-(3,'Inciso C',2018,2000.00,1),
-(4,'Inciso A',2019,3000.00,1),
-(5,'Inciso B',2018,1.00,2),
-(6,'Inciso A',2019,1.00,2);
+(1,'Inciso A',1),
+(2,'Inciso B',1),
+(3,'Inciso A',2),
+(4,'Inciso C',2);
